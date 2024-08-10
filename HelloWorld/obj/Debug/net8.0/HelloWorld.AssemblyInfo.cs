f2a91694e4b26c9aa516e6601cd43c9349a6b9fb
Diegoferpo Portillo <dportillo31@gmail.com>
2024-08-10 16:58:05 -0600

    API de Go

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e0d463e90750a3a90e2c51b11c994a9ffdaf033")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ac7d9a3c8af74c4d9a6b6939a467a0cb7c599d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
